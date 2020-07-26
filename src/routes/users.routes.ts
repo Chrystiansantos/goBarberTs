ff655ec18f1546d8893b2c8079b348b7036cc2f7
Chrystian Santos <chrystiansantos.dev@MacBook-Pro.local>
2020-07-26 10:47:43 -0300

    configurando o multer para fazer upload de avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,13 @@
 import { Router, Request, Response } from 'express';
+import multer from 'multer';
+
 import CreateUserService from '../Service/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+import uploadConfig from '../config/upload';
 
 const userRouter = Router();
+const upload = multer(uploadConfig);
 
 userRouter.post('/', async (req: Request, res: Response) => {
   try {
@@ -14,5 +20,15 @@ userRouter.post('/', async (req: Request, res: Response) => {
     return res.status(400).json({ error: error.message });
   }
 });
+// Path utililzo somente quando quero atualizar apenas um campo
+userRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (req: Request, res: Response) => {
+    console.log(req.file);
+    return res.json({ ok: true });
+  },
+);
 
 export default userRouter;
