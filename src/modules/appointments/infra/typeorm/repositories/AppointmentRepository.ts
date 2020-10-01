c6a9684b3f21bd0f5a06de32297b9993616905e9
Chrystian Santos <chrystiansantos.dev@MacBook-Pro.local>
2020-09-30 23:45:50 -0300

    Liskov SUbstituion Principle

--- src/modules/appointments/infra/typeorm/repositories/AppointmentRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentRepository.ts
@@ -1,15 +1,17 @@
 import { EntityRepository, Repository } from 'typeorm';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import IAppointmentRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 
 // Como parametro preciso informar o meu model
 @EntityRepository(Appointment)
 // Como parrametro do Repository preciso informar o Model
-class AppointmentRepository extends Repository<Appointment> {
-  public async findByDate(date: Date): Promise<Appointment | null> {
+class AppointmentRepository extends Repository<Appointment>
+  implements IAppointmentRepository {
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
     const findAppointment = await this.findOne({
       where: { date },
     });
-    return findAppointment || null;
+    return findAppointment;
   }
 }
 
