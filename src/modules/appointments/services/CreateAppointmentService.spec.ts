98456bd822d1f31aa9bce00266704cd9804e7494
Chrystian Santos <chrystiansantos.dev@MacBook-Pro.local>
2020-10-08 21:04:59 -0300

    FInalizando teste de criacao de agendamento

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,4 +1,5 @@
 import FakeAppointmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import AppError from '@shared/errors/AppError';
 import CreateAppointmentService from './CreateAppointmentService';
 // describe cria como se  fosse uma categoria pra facilitar a vizualizaca dos resultados
 // geralmente passo o nome do service sem o service
@@ -16,7 +17,24 @@ describe('CreateAppointment', () => {
     expect(appointment).toHaveProperty('id');
     expect(appointment.provider_id).toBe('123123');
   });
-  // it('should not be able to create two appointments on the same time', () => {
-  //   expect(1 + 2).toBe(3);
-  // });
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const appointmentDate = new Date();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    await createAppointmentService.execute({
+      date: appointmentDate,
+      provider_id: '123123',
+    });
+    // Dessa forma eu espero que a funcao rejeite um erro e esse erro seja
+    // uma instance de AppError
+    expect(
+      createAppointmentService.execute({
+        date: appointmentDate,
+        provider_id: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
