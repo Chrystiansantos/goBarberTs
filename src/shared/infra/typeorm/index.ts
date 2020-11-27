25fcf6523d06412e1292f4a60201ddc2657f898a
Chrystian Santos <chrystiansantos.dev@MacBook-Pro.local>
2020-11-26 23:01:04 -0300

    Estrutura de notificacoes adicionando e configurando mongo

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,4 +1,4 @@
-import { createConnection } from 'typeorm';
+import { createConnections } from 'typeorm';
 
 // esse metodo ira pesquisar o arquivo ormConfig.json que esta na raiz do projeto
-createConnection();
+createConnections();
